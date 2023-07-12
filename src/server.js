e123f0bdc85d6fd0cde4858476bcf83cb95fb561
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-07-12 09:47:47 +0530

    userRoute: register

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("../src/routes/userRoute");
+
 
 const app = express();
 
@@ -12,9 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
-require('./models/userModel')
-app.use(require('./routes/authentication'))
+
+// Routes Middleware
+app.use("/api/users", userRoute);
 
 // Routes
 app.get("/", (req, res) => {
