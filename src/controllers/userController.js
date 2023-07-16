ebd4263a09395e90d2c18269fc390a477bdfb6f8
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-07-16 09:57:09 +0530

    Login: Route and controller-function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User  = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {expiresIn: "1d"})
@@ -63,6 +64,51 @@ const registerUser = asyncHandler(async (req, res) => {
 
 });
 
+// +++++++++++ Login User ++++++++++++++++++++++
+const loginUser = asyncHandler( async (req, res) => {
+    const {email, password} = req.body;
+
+    // Validate Request
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please add email and password");
+    }
+
+    // Check if user exist
+    const user = await User.findOne({email})
+
+    if (!user) {
+        res.status(400);
+        throw new Error("User not found, please signup");
+    }
+
+    // User exists, check if password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    // Generate Tokens
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400), //1 day
+        sameSite: "none",
+        secure: true,
+    });
+
+    if (user && passwordIsCorrect) {
+        const {_id, name, email, role, photo, phone, } = user
+        res.status(200).json({ //200 is okay or successfull response
+            _id, name, email, photo, role, phone, token,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password");
+    }
+
+});
+
 module.exports = {
-    registerUser,
+    registerUser, loginUser,
 }
