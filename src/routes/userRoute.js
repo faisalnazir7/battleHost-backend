41af8b1ec9f8a407a5d95923bc0dcc5cc7255a45
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-07-20 16:48:17 +0530

    login status

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { registerUser, loginUser, logoutUser, getUser } = require('../controllers/userController');
+const { registerUser, loginUser, logoutUser, getUser, loginStatus } = require('../controllers/userController');
 const protect = require('../middleware/authMiddleware');
 const router=express.Router();
 
@@ -7,5 +7,6 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.post('/getuser',protect, getUser);
+router.get("/loggedin", loginStatus);
 
 module.exports=router
