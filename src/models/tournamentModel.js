36bd4537c99283dffd76ec40c949272447e36e15
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-07-25 18:27:29 +0530

    tournamentModel

---
+++ src/models/tournamentModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+// Define the schema for the Tournament model
+const tournamentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  organizerId: { // role: host
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  }],
+  startDateTime: {
+    type: Date,
+    required: true,
+  },
+  endDateTime: {
+    type: Date,
+    required: true,
+  },
+  rules: {
+    type: String,
+    required: true,
+  },
+  prizes: [{
+    name: String,
+    description: String,
+  }],
+  // Add other fields specific to your application
+}, {
+  timestamps: true, // Automatically adds createdAt and updatedAt fields
+});
+
+// Create the Tournament model from the schema
+const Tournament = mongoose.model('Tournament', tournamentSchema);
+
+module.exports = Tournament;
