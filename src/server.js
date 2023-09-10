4c6dec10a57f88a7fb0c683ed0bb89a3b447aedd
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-09-10 17:51:25 +0530

    CORS: deployed

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:5173","http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173","http://127.0.0.1:5173","https://battle-host-frontend.vercel.app/"],
     credentials: true,
   }))
 // Routes Middleware
